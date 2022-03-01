3d1103df468ed78599b0ea1ea64f830db9b88394
bogialmasi <boglarkaalmasi@gmail.com>
2022-03-01 10:48:16 +0100

    server elkezdve- clientben az App.js function helyett osztály lett (nincs tesztelve)

--- client/bedhunter/src/App.js
+++ client/bedhunter/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 
 import Home from "./pages/Home";
@@ -12,7 +12,54 @@ import Navbar from "./components/Navbar";
 
 import { Switch, Route } from "react-router-dom";
 
+class App extends Component {
+  state = {
+      data: null
+    };
+  
+    componentDidMount() {
+      this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
+      
+    callBackendAPI = async () => {
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      return body;
+    };
+  
+    render() {
+      return (
+        <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route exact path="/promotions/" component={Promotions} />
+        <Route exact path="/contact/" component={Contact} />
+        <Route component={Error} />
+        <p className="App-intro">{this.state.data}</p> 
+      </Switch>
+    </>
+      );
+    }
+  }
 
+  /*
+  <p className="App-intro">{this.state.data}</p>
+  nem tudom mit csinál pontosan ! */
+  
+  export default App;
+
+/*
+
+function-os megoldás, később kellhet még
 
 function App() {
   return (
@@ -30,4 +77,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@
 
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('listening on port ' + port));
 
 app.get('express_backend', (req, res) => {
     res.send({express: 'connected to react'})
+})
+
+app.get('/', function (req, res) {
+    const html = path.join(__dirname, 'view/index.html')
+    res.sendFile(html)
+})
+
+app.get('/style', function (req, res) {
+    res.sendFile() // style.css helye kell ide
+})
+
+app.get('*', function (req, res) {
+    res.redirect('/');
 })
