95030bfd77b4fb845232bdcf4e43024bc265a812
bogialmasi <71004475+bogialmasi@users.noreply.github.com>
2022-03-08 11:56:27 +0100

    mongodb létrehozva, index.js módosítva
    mongodb teszt adatbázissal létrehozva, 
    index.js megjeleníti a bedhunter-ui oldalainak kódját

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 // https://www.section.io/engineering-education/how-to-setup-nodejs-express-for-react/ <--- innen nézem - bogi
 // https://dev.to/nburgess/creating-a-react-app-with-react-router-and-an-express-backend-33l3
+// axios, jwt token
+// https://www.section.io/engineering-education/how-to-build-authentication-api-with-jwt-token-in-nodejs/
+
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 app.listen(port, () => console.log('listening on port ' + port));
-
+/*
 app.get('express_backend', (req, res) => {
     res.send({express: 'connected to react'})
-})
+})*/
 
 app.get('/', function (req, res) {
-    const html = path.join('../client/bedhunter/src/pages/Home.js')
+    const html = path.join('C:/Users/20g_almasib/bedhunter/bedhunter-ui/bedhunter-ui/src/pages/Home.js')
     res.sendFile(html)
 })
 
 app.get('/contact', function (req, res) {
-    const html = path.join('../client/bedhunter/src/pages/Contact.js')
+    const html = path.join('C:/Users/20g_almasib/bedhunter/bedhunter-ui/bedhunter-ui/src/pages/Contact.js')
     res.sendFile(html)
 })
 
 app.get('/promotions', function (req, res) {
-    const html = path.join('../client/bedhunter/src/pages/Promotions.js')
+    const html = path.join('C:/Users/20g_almasib/bedhunter/bedhunter-ui/bedhunter-ui/src/pages/Promotions.js')
     res.sendFile(html)
 })
 
 app.get('/rooms/', function (req, res) {
-    const html = path.join('../client/bedhunter/src/pages/Rooms.js')
+    const html = path.join('C:/Users/20g_almasib/bedhunter/bedhunter-ui/bedhunter-ui/src/pages/Rooms.js')
     res.sendFile(html)
 })
 
 app.get('/singleroom', function (req, res) {
-    const html = path.join('../client/bedhunter/src/pages/SingleRoom.js')
+    const html = path.join('C:/Users/20g_almasib/bedhunter/bedhunter-ui/bedhunter-ui/src/pages/SingleRoom.js')
     res.sendFile(html)
 }) // ez még nem működik
 
@@ -43,6 +47,6 @@ app.get('/style', function (req, res) {
 */
 
 app.get('*', function (req, res) {
-    const html = path.join('../client/bedhunter/src/pages/Error.js')
+    const html = path.join('C:/Users/20g_almasib/bedhunter/bedhunter-ui/bedhunter-ui/src/pages/Error.js')
     res.sendFile(html)
 })
