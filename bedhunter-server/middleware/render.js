3135d4621d7f88fbc487208420a6e0fa47b0821a
bogialmasi <boglarkaalmasi@gmail.com>
2022-04-06 11:59:11 +0200

    jegyzetelés a renderjs fájlba

--- bedhunter-server/middleware/render.js
+++ bedhunter-server/middleware/render.js
@@ -6,3 +6,18 @@ module.exports = (objRep, ejsTemplate) => {
         return res.render(ejsTemplate,  res.locals );
     }
 }
+
+/** RENDER?
+ * res.locals végig él, érkezik, kell next
+ * globális változó? objRepo-ba bemegy (legegyszerűbb), 
+ * VAGY app.locals (gyűjtőhely) keres, kell next
+ * VAGY session
+ * 
+ * res.redirect('lista', req, res) új vonalra lép, nem lesz az előző res, req, next -- undefined lesz 
+ * 
+ * renderMW megjelenít dolgokat
+ * ha a szerver kommunikál a klienssel akkor kell
+ * 
+ * body-parser:kap egy kérést amiből JSONt csinál, aztán JSONt ad vissza
+ * https://stackoverflow.com/questions/50833719/sending-a-string-from-node-js-to-a-component-react-js
+*/
